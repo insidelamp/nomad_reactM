469f198669a1c1211ae20dc2723eb48658c6076e
insidelamp <rkdhfh@naver.com>
2022-05-30 23:09:23 +0900

    문제 해결 전

--- nomad_reactmaster/src/api.ts
+++ nomad_reactmaster/src/api.ts
@@ -5,9 +5,7 @@
 // }
 
 // 위처럼 사용해도됨
-
 const BASE_URL = `https://api.coinpaprika.com/v1`;
-
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
@@ -18,9 +16,19 @@ export function fetchCoinInfo(coinId: string) {
     response.json()
   );
 }
-
 export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
 }
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
+// Math.ceil -> 숫자가 1.9일경우 오름차순해서 2.0 이 보이게됨
+// Math.floor -> 숫자가 1.9일경우 내림차순해서 1.0 이 보이게됨
+//30번째 설명 endDate - 60초 * 60분 * 24시간 * 7일 * 2주
--- nomad_reactmaster/src/routes/Chart.tsx
+++ nomad_reactmaster/src/routes/Chart.tsx
@@ -1,5 +1,72 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ReactApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          //series = 차트에 표시하려는 데이터 ( Array ) (기본값 undefined)
+          options={{
+            // 옵션 차트의 구성옵션 ( Object ) ( 기본값 {})
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              // width, height  차트의 너비 and 높이 (String || Number ) (너비:  기본값 100%) (높이 :기본값 auto)
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
