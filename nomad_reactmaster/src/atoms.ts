bd9a50fef285b050f68cc7613b31825e79503039
insidelamp <rkdhfh@naver.com>
2022-06-07 23:28:59 +0900

    컴포넌트 나누기

--- nomad_reactmaster/src/atoms.ts
+++ nomad_reactmaster/src/atoms.ts
@@ -1,1 +1,12 @@
 import { atom } from "recoil";
+
+export interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
