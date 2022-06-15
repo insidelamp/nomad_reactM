1cd2820e07555223e0890f44471c30da3160a202
insidelamp <rkdhfh@naver.com>
2022-06-15 20:02:53 +0900

    색상추가 및 카드간의 이동

--- nomad_reactmaster/src/atoms.ts
+++ nomad_reactmaster/src/atoms.ts
@@ -1,6 +1,15 @@
 import { atom, selector } from "recoil";
 
-export const toDoState = atom({
+interface IToDoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: ["a", "b", "c", "d", "e", "f"],
+  default: {
+    "To Do": ["a", "b", "c", "d", "e"],
+    doing: ["c", "d", "e"],
+    done: ["f"],
+    "Do Later": ["x", "z"],
+  },
 });
